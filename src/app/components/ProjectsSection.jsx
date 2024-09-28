0ff599165e42ffd781d3f259d024ee5d37bf4549
Jhonny <111026602+jhonnycodedev@users.noreply.github.com>
2024-09-28 13:57:56 -0300

    ok

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -37,7 +37,7 @@ const projectsData = [
     id: 4,
     title: "Now Barber Application",
     description: "Projeto de um localizador de Barbearias para clientes, realizado com Kotlin, Android Studio",
-    image: "/public/images/logo3.jpg",
+    image: "/public/images/barber.png",
     tag: ["All", "Mobile"],
     gitUrl: "https://github.com/jhonnycodedev/AppBarber",
     previewUrl: "/",
