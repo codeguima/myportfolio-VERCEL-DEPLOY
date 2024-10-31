66f738236843fcc34f29730d98c8a14886e43953
Jhonny Guimarães <111026602+jhonnycodedev@users.noreply.github.com>
2024-10-31 19:51:48 -0300

    Atualizar o HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -69,15 +69,17 @@ const HeroSection = () => {
             >
               Contate-Me
             </Link>
+            {/*
             <Link
               href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
               onClick={handleDownloadClick} //button evento para download pdf
-            >{/*sequiser borda no botão text-white border border-white */}
+            >{/*sequiser borda no botão text-white border border-white 
               <span className="block bg-tertiary hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
             </Link>
+            */}
           </div>
         </motion.div>
         <motion.div
